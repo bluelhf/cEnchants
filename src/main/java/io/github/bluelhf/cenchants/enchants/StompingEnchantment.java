d70b59bc9deb7ac291708d7a369887a8baa42959
bluelhf <blueloveshisfriends@gmail.com>
2020-09-13 03:18:04 +0300

    Minor patches to StompingEnchantment

--- src/main/java/io/github/bluelhf/cenchants/enchants/StompingEnchantment.java
+++ src/main/java/io/github/bluelhf/cenchants/enchants/StompingEnchantment.java
@@ -1,30 +1,25 @@
 package io.github.bluelhf.cenchants.enchants;
 
 import io.github.bluelhf.cenchants.cEnchants;
-import io.github.bluelhf.cenchants.utilities.ProjectileUtil;
-import io.github.bluelhf.cenchants.utilities.Quaternion;
+import io.github.bluelhf.cenchants.utilities.EnchantUtil;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-
-import java.util.Collection;
-
-import org.bukkit.*;
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Sound;
 import org.bukkit.enchantments.EnchantmentTarget;
-import org.bukkit.entity.AbstractArrow;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
@@ -34,35 +29,36 @@ public class StompingEnchantment extends CEnchantment implements Listener {
     public void onFall(EntityDamageEvent ev) {
     	
         if (ev.getCause() != EntityDamageEvent.DamageCause.FALL) return;
-        if (!(ev.getEntity() instanceof LivingEntity)) return;
-        Entity entity = (Entity) ev.getEntity();
-        if (EnchantUtil.getEnchantment(entity, this) == null) return;
-        int level = EnchantUtil.getEnchantment(entity, this).getEnchantmentLevel(this);
+        if (!(ev.getEntity() instanceof Player)) return;
+        Player pl = (Player) ev.getEntity();
+        if (EnchantUtil.getEnchantment(pl, this) == null) return;
+        int level = EnchantUtil.getEnchantment(pl, this).getEnchantmentLevel(this);
         
         double dmg = ev.getDamage();
         
         double power = dmg*level/3;
         
-        Collection<Entity> victims = entity.getWorld().getNearbyEntities(entity.getLocation(), power, power, power, e -> (e.isOnGround()));
+        Entity[] victims = pl.getWorld().getNearbyEntities(pl.getLocation(), power, power, power, e -> e.isOnGround() && e instanceof org.bukkit.entity.Damageable).toArray(Entity[]::new);
         
-        entity.getWorld().playSound(entity.getLocation(), Sound.BLOCK_ANVIL_HIT, 1, 1.5F);
-        entity.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, (int) (Math.random()*30+30), 3));
-        ItemStack item = EnchantUtil.getEnchantment(entity, this);
+        pl.getWorld().playSound(pl.getLocation(), Sound.BLOCK_ANVIL_HIT, 1, 1.5F);
+        pl.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, (int) (Math.random()*30+30), 3));
+        ItemStack item = EnchantUtil.getEnchantment(pl, this);
         ItemMeta meta = item.getItemMeta();
         if (meta instanceof Damageable) {
             Damageable damageable = (Damageable)meta;
-            damageable.setDamage(damageable.getDamage() + Math.ceil(level));
+            damageable.setDamage((int) (damageable.getDamage() + Math.ceil(level)));
         }
         item.setItemMeta(meta);
         
-        for(int i = 0; i < victims.size(); i ++) {
+        for(int i = 0; i < victims.length; i ++) {
         	Entity target = victims[i];
+            org.bukkit.entity.Damageable d = (org.bukkit.entity.Damageable) target;
+            d.damage(power);
         	
-        	entity.getServer().broadcastMessage(target.getCustomName() + " just got stomped lol");
         	target.getVelocity().add(new Vector(
-        			power*(target.getLocation().getX() - entity.getLocation().getX()), 
-        			power*(target.getLocation().getY() - entity.getLocation().getY()), 
-        			power*(target.getLocation().getZ() - entity.getLocation().getZ())));
+        			power*(target.getLocation().getX() - pl.getLocation().getX()),
+        			power*(target.getLocation().getY() - pl.getLocation().getY()),
+        			power*(target.getLocation().getZ() - pl.getLocation().getZ())));
         }
         
     }
