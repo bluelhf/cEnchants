68e23c4101bcbf1c72ab7f923cb177c668a0d6e6
Blue <blueloveshisfriends@gmail.com>
2020-09-18 22:52:49 +0300

    Remove duplicate maven dependencies, update .gitignore, add recipe caching & general improvements to Smelting Touch

--- src/main/java/io/github/bluelhf/cenchants/enchants/SmeltingTouchEnchantment.java
+++ src/main/java/io/github/bluelhf/cenchants/enchants/SmeltingTouchEnchantment.java
@@ -1,77 +1,49 @@
 package io.github.bluelhf.cenchants.enchants;
 
 import io.github.bluelhf.cenchants.cEnchants;
-import io.github.bluelhf.cenchants.utilities.ProjectileUtil;
-import io.github.bluelhf.cenchants.utilities.Quaternion;
+import io.github.bluelhf.cenchants.utilities.EnchantUtil;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-
-import java.util.Random;
-
-import org.bukkit.*;
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
+import org.bukkit.inventory.Recipe;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class SmeltingTouchEnchantment extends CEnchantment implements Listener {
-	
+
+    private HashMap<Material, Material> smeltMap = new HashMap<>();
+
     public SmeltingTouchEnchantment(NamespacedKey key) {
         super(key);
     }
-    
-    Random random = new Random();
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockBreak(BlockDropItemEvent ev) {
-    	
-    	if (ev.isCancelled()) return;
-    	if (!(ev.getEntity() instanceof Player)) return;
-        Player pl = (Player) ev.getEntity();
-        if (EnchantUtil.getEnchantment(pl, this) == null) return;
-        int level = EnchantUtil.getEnchantment(pl, this).getEnchantmentLevel(this);
-        int fortune = 1;
-        if (EnchantUtil.getEnchantment(pl, this) != null) {
-        	int fortune = 1 + random.nextInt(EnchantUtil.getEnchantment(pl, Enchantment.LOOT_BONUS_BLOCKS).getEnchantmentLevel(this));
+        if (ev.isCancelled()) return;
+        if (EnchantUtil.getEnchantment(ev.getPlayer(), this) == null) return;
+
+        for (int i = 0; i < ev.getItems().size(); i++) {
+            Item drop = ev.getItems().get(i);
+            ItemStack stack = drop.getItemStack();
+            if (smeltMap.containsKey(stack.getType())) stack.setType(smeltMap.get(stack.getType()));
+            drop.setItemStack(stack);
         }
-    	
-    	Item[] drops = ev.getItems().toArray(Item[]::new);
-    	
-    	for (int i = 0; i < drops.length; i ++) {
-    		ItemStack drop = getSmelted(drops[i].getItemStack());
-    		if (drop == null) continue;
-        	drop = new ItemStack(drop.getType(), drop.getAmount() * fortune);
-        	ev.getItems().set(i, drop);
-    		
-    	}
-    	
     }
-    
-    private ItemStack getSmelted(ItemStack input) {
-    	
-    	ItemStack result = null;
-		Iterator<Recipe> iter = Bukkit.recipeIterator();
-		while (iter.hasNext()) {
-		   Recipe recipe = iter.next();
-		   if (!(recipe instanceof FurnaceRecipe)) continue;
-		   if (((FurnaceRecipe) recipe).getInput().getType() != input.getType()) continue;
-		   result = recipe.getResult();
-		   break;
-		}
-		
-		return result;
-		
-    }
-    
+
     @Override
     public BaseComponent[] getDescription() {
         return new ComponentBuilder()
@@ -79,50 +51,57 @@ public class SmeltingTouchEnchantment extends CEnchantment implements Listener {
             .append("smelts things it touches!").reset()
             .create();
     }
-    
+
     @Override
     public void onRegister() {
         Bukkit.getPluginManager().registerEvents(this, cEnchants.get());
+
+        Iterator<Recipe> iter = Bukkit.recipeIterator();
+        while (iter.hasNext()) {
+            Recipe recipe = iter.next();
+            if (!(recipe instanceof FurnaceRecipe)) continue;
+            smeltMap.put(((FurnaceRecipe) recipe).getInput().getType(), recipe.getResult().getType());
+        }
     }
-    
+
     @Override
     public void onUnregister() {
         HandlerList.unregisterAll(this);
     }
-    
+
     @Override
     public @NotNull String getName() {
         return "Smalting Touch";
     }
-    
+
     @Override
     public int getMaxLevel() {
         return 1;
     }
-    
+
     @Override
     public int getStartLevel() {
         return 0;
     }
-    
+
     @Override
     public @NotNull EnchantmentTarget getItemTarget() {
         return EnchantmentTarget.TOOL;
     }
-    
+
     @Override
     public boolean isTreasure() {
         return false;
     }
-    
+
     @Override
     public boolean isCursed() {
         return false;
     }
-    
+
     @Override
     public Rarity getRarity() {
         return Rarity.RARE;
     }
-    
+
 }
